56043e2026733d0ec6537a8373abf53b340e6324
rawriet <rawriet@qq.com>
2026-01-29 21:41:17 +0100

    Fix HIP build script to use canonical scripts dir
    Use scripts/ as SCRIPTSDIR to avoid missing symlinks in fresh clones.
    
    This removes the dependency on scripts_rocm/* symlinks for bound_code.py and related generators.
    CUDA compatibility header remains in scripts_rocm/cuda_compat.h.

--- scripts_rocm/build_hip.sh
+++ scripts_rocm/build_hip.sh
@@ -6,6 +6,6 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
 make -f src/makefile.hip allp SETUP=fargo PARALLEL=0 GPU=1 \
-  SCRIPTSDIR=./scripts_rocm SETUPSDIR=./setups STDDIR=./std SRCDIR=./src ARCHDIR=./arch BINDIR=./bin \
+  SCRIPTSDIR=./scripts SETUPSDIR=./setups STDDIR=./std SRCDIR=./src ARCHDIR=./arch BINDIR=./bin \
   EXENAME=./bin/fargo3d_hip LINKER='gcc -no-pie' \
   CUDAOPT='-include ./scripts_rocm/cuda_compat.h'
